4bf2102506e56730d40076a3198bb5a00c7768a7
ShvedIryna <shvediruna@gmail.com>
2024-11-21 11:29:55 +0200

    added lab9

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer-content">
+                <p>Elite Pools ©. All Rights Reserved.</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
